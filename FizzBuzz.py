80f4d703ccd1e15248161b89a37cd269955683cd
covertseth <40729060+covertseth@users.noreply.github.com>
2019-01-25 16:30:33 -0600

    FizzBuzz Test Solution

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+def main(multiples, *args):
+    for i in range(*args):
+        output = ''
+        for multiple in multiples:
+            if i%multiple==0:
+                output+=multiples[multiple]
+        if output=='':
+            output = i
+        print(output)
+
+multiples = {3:'Fizz' ,5:'Buzz'}
+
+
+
+
+main(multiples, 1,101)
